0ec418aabcc6c2d0e5a826a63ef84c9e69ff0ee3
Abdul Basit <tcomprog@gmail.com>
2025-08-23 18:19:03 +0200

    first commit

---
+++ src/main/java/com/basit/cz/ai_test_cases_generation/AiTestCasesGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.basit.cz.ai_test_cases_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTestCasesGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTestCasesGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basit/cz/ai_test_cases_generation/AiTestCasesGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basit.cz.ai_test_cases_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTestCasesGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
